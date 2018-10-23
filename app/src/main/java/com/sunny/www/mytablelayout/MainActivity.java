3a46bf9b50eada98500c1d568ebb76e59a5739c3
kojomba69 <kojomba@naver.com>
2018-10-23 13:51:21 +0900

    Initial commit

---
+++ app/src/main/java/com/sunny/www/mytablelayout/MainActivity.java
@@ -0,0 +1,33 @@
+package com.sunny.www.mytablelayout;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button)findViewById(R.id.button1);
+        final EditText editText1=(EditText)findViewById(R.id.editText1) ;
+        button1.setBackgroundColor(Color.CYAN);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"행복해요", Toast.LENGTH_SHORT).show();
+                editText1.setText("배고파");
+                v.setBackgroundColor(Color.DKGRAY);
+            }
+
+
+
+        });
+    }
+}
